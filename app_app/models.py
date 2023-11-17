cfe9a0e4418bbaa3c672074f9ea0a05dcea6ac91
eevilevaniemi <eevihelmimaria@gmail.com>
2023-11-17 11:56:32 +0200

    Lisäsin models ja koitan osaanko käyttää gittiä :)

--- app_app/models.py
+++ app_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class BoardGame(models.Model):
+    name = models.CharField(max_length=100)
+    # Add other fields as needed
+
+    def __str__(self):
+        return self.name
+
+class BoardGamer(models.Model):
+    name = models.CharField(max_length=100)
+    # Add other fields as needed
+
+    def __str__(self):
+        return self.name
+
+class GameLoan(models.Model):
+    board_game = models.ForeignKey(BoardGame, on_delete=models.CASCADE)
+    borrower = models.ForeignKey(BoardGamer, on_delete=models.CASCADE)
+    loan_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.board_game} - {self.borrower} - {self.loan_date}"
--- app_env/Scripts/activate.bat
+++ app_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rikuv\Documents\School\applicationprogramming\app_env
+set VIRTUAL_ENV=C:\Users\Eevi\appprog\app_env
 
 if not defined PROMPT set PROMPT=$P$G
 
